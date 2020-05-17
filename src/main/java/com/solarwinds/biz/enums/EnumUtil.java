bc1aba5049e7e94e8778e8985cd56b0b00c3781e
George Wei <gweii@163.com>
2020-05-17 16:08:34 +0800

    EnumUtil: Modify getValueList methods to support customizing mapped key names

--- src/main/java/com/solarwinds/biz/enums/EnumUtil.java
+++ src/main/java/com/solarwinds/biz/enums/EnumUtil.java
@@ -21,45 +21,86 @@ public class EnumUtil {
     public static <T extends Enum<T> & ITypeEnum> T fromDb(Class<T> enumClass, String dbValue) {
         return ITypeEnum.fromDb(enumClass, dbValue);
     }
+
+    public static String KEY_NAME_FOR_VALUE = "valueKeyName";
+    public static String KEY_NAME_FOR_DESC = "descKeyName";
+    private static Map<String, String> DEFAULT_MAPPED_KEYNAMES = new HashMap<String, String>(){{
+        put(KEY_NAME_FOR_VALUE, "code");
+        put(KEY_NAME_FOR_DESC, "name");
+        }};
     /*
-     * Encapsulates all values of an enum and returns as a list of map
+     * Encapsulates all values of an enum and returns as a list of map use default mapped key names
      *
      * @param enumClass The enum class which implements IStatusEnum to be casted
      * @return List contains enum values, each value was encapsulated into a map which has keys named 'code' and 'name'
      */
     public static <T extends Enum<T> & IStatusEnum> List<Map<String, Object>> getValueList(Class<T> enumClass) {
+        return getValueList(enumClass, DEFAULT_MAPPED_KEYNAMES);
+    }
+    /*
+     * Encapsulates all values of an enum and returns as a list of map use customized mapped key names
+     *
+     * @param enumClass The enum class which implements IStatusEnum to be casted
+     * @param mappedKeyNames Customized mapped key names of enum's value & desc properties
+     * @return List contains enum values, each value was encapsulated into a map, the mapped key names are customized by parameter mappedKeyNames
+     */
+    public static <T extends Enum<T> & IStatusEnum> List<Map<String, Object>> getValueList(Class<T> enumClass,
+                                                                                           Map<String, String> mappedKeyNames) {
+        CheckMappedKeyNames(mappedKeyNames);
         T[] values = getEnumValues(enumClass);
 
         List<Map<String, Object>> result = new ArrayList<>();
         for (T value: values) {
-            result.add(enumValueToMap(value.toDb(), value.getDesc()));
+            result.add(enumValueToMap(value.toDb(), value.getDesc(), mappedKeyNames));
         }
 
         return result;
     }
 
     /*
-     * Encapsulates all values of an enum and returns as a list of map
+     * Encapsulates all values of an enum and returns as a list of map use default mapped key names
      *
      * @param enumClass The enum class which implements ITypeEnum to be casted
      * @param dummy A dummy parameter just used to distinguish with another method with the same name, can be any int value
      * @return List contains enum values, each value was encapsulated into a map which has keys named 'code' and 'name'
      */
     public static <T extends Enum<T> & ITypeEnum> List<Map<String, Object>> getValueList(Class<T> enumClass, int dummy) {
+        return getValueList(enumClass, DEFAULT_MAPPED_KEYNAMES, dummy);
+    }
+    /*
+     * Encapsulates all values of an enum and returns as a list of map use customized mapped key names
+     *
+     * @param enumClass The enum class which implements ITypeEnum to be casted
+     * @param mappedKeyNames Customized mapped key names of enum's value & desc properties
+     * @param dummy A dummy parameter just used to distinguish with another method with the same name, can be any int value
+     * @return List contains enum values, each value was encapsulated into a map, the mapped key names are customized by parameter mappedKeyNames
+     */
+    public static <T extends Enum<T> & ITypeEnum> List<Map<String, Object>> getValueList(Class<T> enumClass,
+                                                                                         Map<String, String> mappedKeyNames,
+                                                                                         int dummy) {
+        CheckMappedKeyNames(mappedKeyNames);
         T[] values = getEnumValues(enumClass);
 
         List<Map<String, Object>> result = new ArrayList<>();
         for (T value: values) {
-            result.add(enumValueToMap(value.toDb(), value.getDesc()));
+            result.add(enumValueToMap(value.toDb(), value.getDesc(), mappedKeyNames));
         }
 
         return result;
     }
 
-    private static <T extends Enum<T> & ITypeEnum> Map<String, Object> enumValueToMap(Object dbValue, String desc) {
+    private static void CheckMappedKeyNames(Map<String, String> mappedKeyNames) {
+        if (mappedKeyNames != DEFAULT_MAPPED_KEYNAMES){
+            if (!mappedKeyNames.containsKey(KEY_NAME_FOR_VALUE) ||
+                    !mappedKeyNames.containsKey(KEY_NAME_FOR_DESC))
+                throw new RuntimeException(String.format("Incorrect definition of mapped key names. Keys named '%s' and '%s' required.", KEY_NAME_FOR_VALUE, KEY_NAME_FOR_DESC));
+        }
+    }
+    private static Map<String, Object> enumValueToMap(Object dbValue, String desc,
+                                                      Map<String, String> mappedKeyNames) {
         Map<String, Object> map = new HashMap<>();
-        map.put("code", dbValue);
-        map.put("name", desc);
+        map.put(mappedKeyNames.get(KEY_NAME_FOR_VALUE), dbValue);
+        map.put(mappedKeyNames.get(KEY_NAME_FOR_DESC), desc);
         return map;
     }
 
--- src/test/java/com/solarwinds/biz/enums/EnumUtilTest.java
+++ src/test/java/com/solarwinds/biz/enums/EnumUtilTest.java
@@ -2,6 +2,7 @@ package com.solarwinds.biz.enums;
 
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -12,15 +13,41 @@ public class EnumUtilTest {
     void testToListTypeEnum() {
         List<Map<String, Object>> result = EnumUtil.getValueList(AssetTypeEnum.class, 0);
         assert result.size() == 5;
+
+        for (Map<String, Object> m: result) {
+            String value = (String)m.get("code");
+            switch (value) {
+                case "r":
+                    assert m.get("name").equals("creditor's rights");
+                    break;
+                case "h":
+                    assert m.get("name").equals("house");
+                    break;
+                case "s":
+                    assert m.get("name").equals("stock");
+                    break;
+                case "c":
+                    assert m.get("name").equals("construction in progress");
+                    break;
+                case "l":
+                    assert m.get("name").equals("land");
+                    break;
+                default:
+            }
+        }
     }
 
     @Test
     void testToListStatusEnum() {
-        List<Map<String, Object>> result = EnumUtil.getValueList(FlowStatusEnum.class);
+        List<Map<String, Object>> result = EnumUtil.getValueList(FlowStatusEnum.class,
+                new HashMap<String, String>(){{
+                    put(EnumUtil.KEY_NAME_FOR_VALUE, "type");
+                    put(EnumUtil.KEY_NAME_FOR_DESC, "name");
+                }});
         assert result.size() == 4;
 
         for (Map<String, Object> m: result) {
-            int value = (Integer)m.get("code");
+            int value = (Integer)m.get("type");
             switch (value) {
                 case 1:
                     assert m.get("name").equals("Unsubmitted");
